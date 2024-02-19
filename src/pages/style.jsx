144f44de267feb96a29bb3c6817c31589b88d678
Natan Altomar <89466514+Natanalpe@users.noreply.github.com>
2024-02-19 01:19:49 -0300

    Update style.jsx

--- src/pages/style.jsx
+++ src/pages/style.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const DevLinksWrapper = styled.div`
      width: 100dvw;
-     min-height: 100dvh;
+     min-height: 100vh;
      background-color: #171717;
      display: flex;
      flex-direction: column;
@@ -87,7 +87,7 @@ export const DevLinksWrapper = styled.div`
 
      footer {
           width: 100%;
-          height: 80px;
+          height: 70px;
           border-top: 1px solid #21262d;
           position: absolute;
           bottom: 0;
